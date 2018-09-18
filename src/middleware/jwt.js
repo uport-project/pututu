3f4c1af30b1867ba9e65d86d9d2bfe8ed5de797c
Cristóbal Castillo <cristobal.castillo@consensys.net>
2018-09-18 13:45:37 -0300

    log issProfile

--- src/middleware/jwt.js
+++ src/middleware/jwt.js
@@ -7,17 +7,24 @@ const IAT_SKEW = 60
 
 function JwtDecode (req, res, next) {
   let token
-  let log = log4js.getLogger('pututu.middleware.jwt');
-  log.setLevel('INFO');
-  let logData= {
+  let log = log4js.getLogger('pututu.middleware.jwt')
+  log.setLevel('INFO')
+  let logData = {
     id: req.id
   }
 
-  if (req.method === 'OPTIONS' && req.headers.hasOwnProperty('access-control-request-headers')) {
-    var hasAuthInAccessControl = !!~req.headers['access-control-request-headers']
-                                    .split(',').map(function (header) {
-                                      return header.trim()
-                                    }).indexOf('authorization')
+  if (
+    req.method === 'OPTIONS' &&
+    req.headers.hasOwnProperty('access-control-request-headers')
+  ) {
+    var hasAuthInAccessControl = !!~req.headers[
+      'access-control-request-headers'
+    ]
+      .split(',')
+      .map(function (header) {
+        return header.trim()
+      })
+      .indexOf('authorization')
 
     if (hasAuthInAccessControl) {
       return next()
@@ -33,15 +40,15 @@ function JwtDecode (req, res, next) {
       if (/^Bearer$/i.test(scheme)) {
         token = credentials
       } else {
-        let err={ message: 'Format is Authorization: Bearer [token]' }
-        logData.err=err;
-        log.error(logData);
+        let err = { message: 'Format is Authorization: Bearer [token]' }
+        logData.err = err
+        log.error(logData)
         return res.status(403).json(err)
       }
     } else {
-      let err={ message: 'Format is Authorization: Bearer [token]' }
-      logData.err=err;
-      log.error(logData);
+      let err = { message: 'Format is Authorization: Bearer [token]' }
+      logData.err = err
+      log.error(logData)
       return res.status(403).json(err)
     }
   }
@@ -53,91 +60,101 @@ function JwtDecode (req, res, next) {
     try {
       dtoken = decodeToken(token)
     } catch (decodeErr) {
-      logData.decodeErr=decodeErr;
-      let err={ message: 'Invalid JWT token' }
-      logData.err=err;
-      log.error(JSON.stringify(logData));
+      logData.decodeErr = decodeErr
+      let err = { message: 'Invalid JWT token' }
+      logData.err = err
+      log.error(JSON.stringify(logData))
       return res.status(403).json(err)
     }
 
-    logData.dtoken=dtoken;
+    logData.dtoken = dtoken
 
-    //Check issuedAt
-    if (dtoken.payload.iat && ((dtoken.payload.iat >=LEGACY_MS && dtoken.payload.iat > Date.now() + (IAT_SKEW*1000)) ||
-                               (dtoken.payload.iat < LEGACY_MS && dtoken.payload.iat > Date.now() / 1000 + IAT_SKEW))) {
-      //console.log("JWT issued in the future?. JWT:"+dtoken.payload.exp+" Now:"+new Date().getTime())
-      let err = { message: `JWT not valid yet (issued in the future) : iat: ${dtoken.payload.iat} > now: ${Date.now() / 1000}`}
-      logData.err=err;
-      log.error(JSON.stringify(logData));
+    // Check issuedAt
+    if (
+      dtoken.payload.iat &&
+      ((dtoken.payload.iat >= LEGACY_MS &&
+        dtoken.payload.iat > Date.now() + IAT_SKEW * 1000) ||
+        (dtoken.payload.iat < LEGACY_MS &&
+          dtoken.payload.iat > Date.now() / 1000 + IAT_SKEW))
+    ) {
+      // console.log("JWT issued in the future?. JWT:"+dtoken.payload.exp+" Now:"+new Date().getTime())
+      let err = {
+        message: `JWT not valid yet (issued in the future) : iat: ${dtoken.payload.iat} > now: ${Date.now() / 1000}`
+      }
+      logData.err = err
+      log.error(JSON.stringify(logData))
       return res.status(403).json(err)
     }
 
-    //Check expireAt
-    if (dtoken.payload.exp && ((dtoken.payload.exp >=LEGACY_MS && dtoken.payload.exp <= Date.now()) ||
-                               (dtoken.payload.iat < LEGACY_MS && dtoken.payload.exp <= Date.now() / 1000))) {
-      //console.log("JWT expired. JWT:"+dtoken.payload.exp+" Now:"+new Date().getTime())
-      let err = { message: `JWT has expired: exp: ${dtoken.payload.exp} < now: ${Date.now() / 1000}`}
-      logData.err=err;
-      log.error(JSON.stringify(logData));
+    // Check expireAt
+    if (
+      dtoken.payload.exp &&
+      ((dtoken.payload.exp >= LEGACY_MS && dtoken.payload.exp <= Date.now()) ||
+        (dtoken.payload.iat < LEGACY_MS &&
+          dtoken.payload.exp <= Date.now() / 1000))
+    ) {
+      // console.log("JWT expired. JWT:"+dtoken.payload.exp+" Now:"+new Date().getTime())
+      let err = {
+        message: `JWT has expired: exp: ${dtoken.payload.exp} < now: ${Date.now() / 1000}`
+      }
+      logData.err = err
+      log.error(JSON.stringify(logData))
       return res.status(403).json(err)
     }
 
-    //Get profile.publicKey from uport-registry for iss and validate token signature
-    //TODO: Replace with JWT.verifyJWT() from uport-project
+    // Get profile.publicKey from uport-registry for iss and validate token signature
+    // TODO: Replace with JWT.verifyJWT() from uport-project
     const registry = UportLite()
 
     registry(dtoken.payload.iss, (registryErr, issProfile) => {
-      if(Object.keys(registryErr).length > 0){
-        logData.registryErr=registryErr;
-        let err={ message: 'Error getting profile' }
-        logData.err=err;
-        log.error(JSON.stringify(logData));
+      if (Object.keys(registryErr).length > 0) {
+        logData.registryErr = registryErr
+        let err = { message: 'Error getting profile' }
+        logData.err = err
+        log.error(JSON.stringify(logData))
         return res.status(500).json(err)
       }
-      if(!issProfile){
-        let err={ message: 'Profile not found, unable to verify token' }
-        logData.err=err;
-        log.error(JSON.stringify(logData));
-        return res.status(500).json(err);
+      console.log('issProfile', issProfile)
+      if (!issProfile) {
+        let err = { message: 'Profile not found, unable to verify token' }
+        logData.err = err
+        log.error(JSON.stringify(logData))
+        return res.status(500).json(err)
       }
-      const publicKey = issProfile.publicKey.match(/^0x/) ? issProfile.publicKey.slice(2) : issProfile.publicKey
+      const publicKey = issProfile.publicKey.match(/^0x/)
+        ? issProfile.publicKey.slice(2)
+        : issProfile.publicKey
       let verified = new TokenVerifier('ES256k', publicKey).verify(token)
 
       if (!verified) {
-        let err={ message: 'Invalid signature in JWT token' }
-        logData.err=err;
-        log.error(JSON.stringify(logData));
-        return res.status(500).json(err);
+        let err = { message: 'Invalid signature in JWT token' }
+        logData.err = err
+        log.error(JSON.stringify(logData))
+        return res.status(500).json(err)
       } else {
-        if(dtoken.payload.aud){
-
-          //Search for aud data
-          registry(dtoken.payload.aud , (audErr, audProfile) =>{
-            if(!audErr){
-              dtoken.payload.audName=audProfile.name;
-            }else{
-              let err="ERROR trying to get profile for aud:"+dtoken.payload.aud;
-              logData.err=err;
-              log.error(JSON.stringify(logData));
+        if (dtoken.payload.aud) {
+          // Search for aud data
+          registry(dtoken.payload.aud, (audErr, audProfile) => {
+            if (!audErr) {
+              dtoken.payload.audName = audProfile.name
+            } else {
+              let err =
+                'ERROR trying to get profile for aud:' + dtoken.payload.aud
+              logData.err = err
+              log.error(JSON.stringify(logData))
             }
             req.authorization = dtoken.payload
             return next()
           })
-
-        }else{
+        } else {
           req.authorization = dtoken.payload
           return next()
         }
-
-
       }
-
     })
-
-  }else{
-    return next();
+  } else {
+    return next()
   }
-
 }
 
 module.exports = JwtDecode
